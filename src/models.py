ff2f33e11ce0bd65a87a36f6b5fa94a8fa63618b
IvanStacy <139801928+IvanStacy@users.noreply.github.com>
2023-12-30 18:04:42 +0000

    done

--- src/models.py
+++ src/models.py
@@ -1,32 +1,58 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from sqlalchemy.sql import func
 
+db = SQLAlchemy(app)
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'users'
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    registration_date = Column(DateTime, server_default=func.now())
+    favorites = relationship('Favorite', back_populates='user')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+class Character(Base):
+    __tablename__ = 'characters'
+    character_id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    species = Column(String(50))
+    homeworld = Column(String(100))
+    affiliation = Column(String(100))
+    description = Column(String(500))
+    favorites = relationship('Favorite', back_populates='character')
+
+class Planet(Base):
+    __tablename__ = 'planets'
+    planet_id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    climate = Column(String(100))
+    terrain = Column(String(100))
+    population = Column(Integer)
+    description = Column(String(500))
+
+    # Relationship to Favorite Model
+    favorites = relationship('Favorite', back_populates='planet')
+
+class Favorite(Base):
+    __tablename__ = 'favorites'
+    favorite_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
+    character_id = Column(Integer, ForeignKey('characters.character_id'))
+    planet_id = Column(Integer, ForeignKey('planets.planet_id'))
+    favorite_date = Column(DateTime, server_default=func.now())
+
+    # Relationships to User, Character, and Planet Models
+    user = relationship('User', back_populates='favorites')
+    character = relationship('Character', back_populates='favorites')
+    planet = relationship('Planet', back_populates='favorites')
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
