d1548577d70da76cd35c46600337d97d5baa31fd
IvanStacy <139801928+IvanStacy@users.noreply.github.com>
2024-01-03 16:10:11 +0000

    update

--- src/models.py
+++ src/models.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from eralchemy2 import render_er
 from sqlalchemy.sql import func
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 Base = declarative_base()
 
 class User(Base):
@@ -16,7 +16,7 @@ class User(Base):
     email = Column(String(100), unique=True, nullable=False)
     password = Column(String(100), nullable=False)
     registration_date = Column(DateTime, server_default=func.now())
-    favorites = relationship('Favorite', back_populates='user')
+    favorites = relationship('Favorites', back_populates='user')
 
 
 class Character(Base):
@@ -27,7 +27,7 @@ class Character(Base):
     homeworld = Column(String(100))
     affiliation = Column(String(100))
     description = Column(String(500))
-    favorites = relationship('Favorite', back_populates='character')
+    favorites = relationship('Favorites', back_populates='character')
 
 class Planet(Base):
     __tablename__ = 'planets'
@@ -39,20 +39,30 @@ class Planet(Base):
     description = Column(String(500))
 
     # Relationship to Favorite Model
-    favorites = relationship('Favorite', back_populates='planet')
+    favorites = relationship('Favorites', back_populates='planet')
 
-class Favorite(Base):
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    cost_in_credits = Column(Integer, nullable=False)
+    model = Column(String(250), nullable=False)
+    favorites = relationship("Favorites", back_populates="Starships")
+
+
+class Favorites(Base):
     __tablename__ = 'favorites'
     favorite_id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
     character_id = Column(Integer, ForeignKey('characters.character_id'))
     planet_id = Column(Integer, ForeignKey('planets.planet_id'))
+    starships_id = Column(Integer, ForeignKey("starships.id"))
     favorite_date = Column(DateTime, server_default=func.now())
 
     # Relationships to User, Character, and Planet Models
     user = relationship('User', back_populates='favorites')
     character = relationship('Character', back_populates='favorites')
     planet = relationship('Planet', back_populates='favorites')
-
+    starships = relationship("Starships", back_populates="Favorites")
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
